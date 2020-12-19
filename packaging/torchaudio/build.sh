864e465a713f34093671c84f8a73f589efa7b9fb
Vincent Quenneville-Belair <vincentqb@gmail.com>
2020-12-18 17:29:37 -0800

    remove environment variable for conda.

--- packaging/torchaudio/build.sh
+++ packaging/torchaudio/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
-export LD_LIBRARY_PATH="$CUDA_HOME/extras/CUPTI/lib64:$LD_LIBRARY_PATH"
-export LIBRARY_PATH=$CUDA_HOME/lib64:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
-export CFLAGS="-I$CUDA_HOME/include $CFLAGS"
+# export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+# export LD_LIBRARY_PATH="$CUDA_HOME/extras/CUPTI/lib64:$LD_LIBRARY_PATH"
+# export LIBRARY_PATH=$CUDA_HOME/lib64:$LIBRARY_PATH
+# export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+# export CFLAGS="-I$CUDA_HOME/include $CFLAGS"
 
 BUILD_SOX=1 python setup.py install --single-version-externally-managed --record=record.txt
